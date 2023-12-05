137eedb6d666369ac0cb95f91a06f1c518afe6a6
Kalpana Gunashekar <emb-kalpgun@embitel.com>
2023-12-05 14:22:27 +0530

    implemented Screenshot and report generation

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,24 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter', // for html report
   e2e: {
     watchForFileChanges : false,
+    screenshotOnRunFailure : true,
+  
+  
     setupNodeEvents(on, config) {
+
+      require('cypress-mochawesome-reporter/plugin')(on);
+      
+
+
+
       // implement node event listeners here
     },
+
+    
   },
 });
+
+
